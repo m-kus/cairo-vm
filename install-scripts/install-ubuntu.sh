c54972e11bd8b81fff64cd6545c9c148f5ba8e79
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-05-12 15:25:59 -0300

    Minor fixes to install-ubuntu.sh

--- install-scripts/install-ubuntu.sh
+++ install-scripts/install-ubuntu.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-PYTHON_VERSION=3.9.15
-PYTHON_MINOR_VERSION=${PYTHON_VERSION%.*}
-
 sudo apt update -y
 
 # Install Rust and Cargo
@@ -12,11 +9,6 @@ source "$HOME/.cargo/env"
 
 # Make sure Rust has been installed correctly
 rustc --version
-
-# Install uv dependencies
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-        libreadline-dev libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev \
-        xz-utils tk-dev libffi-dev liblzma-dev libgmp3-dev
 
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
